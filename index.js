f194e7ffbc0dd3df7360478c3e48070f4f281c81
Daniel Dauber <ddauberox@gmail.com>
2020-07-22 13:36:46 -0300

    update function

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const axios = require("axios");
+let url = process.env.URL
 
 exports.handler = async function (event, context) {
   event.Records.forEach(async (record) => {
     const { body, messageAttributes } = record;
-
-    const path = messageAttributes.path.stringValue;
-    const client_id = messageAttributes.cliente_id.stringValue;
-    const doc_id = messageAttributes.doc_id.stringValue;
-    const token = messageAttributes.token.stringValue;
-
-    console.log(`Attributes: ${path}`);
-    console.log(`Attributes: ${client_id}`);
-    console.log(`Attributes: ${doc_id}`);
-    console.log(`Attributes: ${token}`);
+    
+    console.log(messageAttributes);
+    const param = messageAttributes.path.stringValue;
 
     await axios
       .post(
-        path,
+        `${url}`,
         {
-          client_id,
-          doc_id,
+          param,
         },
         {
           headers: {
